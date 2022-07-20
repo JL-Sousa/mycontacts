dfb703aeef87098fb718e7e6f5cb0f4145ccf746
Jonyelson Lopes <jonyelson20@gmail.com>
2022-07-20 16:11:15 -0300

    Criado page home e refatorado o header

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -4,27 +4,21 @@ export const Container = styled.div`
     margin-top: 32px;
 `;
 
-export const Header = styled.header`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+export const InputSearchContainer = styled.div`
+    width: 100%;
 
-    strong {
-        font-size: 24px;
-    }
+    input {
+        width: 100%;
+        height: 50px;
+        background: #fff;
+        border: none;
+        border-radius: 25px;
+        box-shadow: 0px 4px 10px rgba(0 , 0, 0, 0.4);
+        outline: 0;
+        padding: 0 16px;
 
-    a {
-        color: ${({ theme }) => theme.colors.primary.main};
-        text-decoration: none;
-        font-weight: bold;
-        border: 2px solid ${({ theme }) => theme.colors.primary.main};
-        padding: 8px 16px;
-        border-radius: 4px;
-        transition: all 0.2s ease-in;
-
-        &:hover {
-            background: ${({ theme }) => theme.colors.primary.main};
-            color: #fff;
+        &::placeholder {
+            color: #bcbcbc;
         }
     }
 `;
@@ -46,6 +40,32 @@ export const ListContainer = styled.div`
             font-weight: bold;
             color: ${({ theme }) => theme.colors.primary.main};
         }
+        }
+    }
+`;
+
+export const Header = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 32px;
+
+    strong {
+        font-size: 24px;
+    }
+
+    a {
+        color: ${({ theme }) => theme.colors.primary.main};
+        text-decoration: none;
+        font-weight: bold;
+        border: 2px solid ${({ theme }) => theme.colors.primary.main};
+        padding: 8px 16px;
+        border-radius: 4px;
+        transition: all 0.2s ease-in;
+
+        &:hover {
+            background: ${({ theme }) => theme.colors.primary.main};
+            color: #fff;
         }
     }
 `;
