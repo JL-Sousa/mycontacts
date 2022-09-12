9c704801243c614eaa578826100d8acec16a80fa
Jonyelson Lopes <jonyelson20@gmail.com>
2022-09-12 16:57:23 -0300

    Componente Button

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,6 +1,7 @@
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
+import Button from '../../components/Button';
 
 export default function NewContact() {
   return (
@@ -16,6 +17,8 @@ export default function NewContact() {
         <option value="123">Facebook</option>
         <option value="123">Instagram</option>
       </Select>
+      <Button type="button">Salvar alteracoes</Button>
+      <Button type="button" disabled>Salvar alteracoes</Button>
     </>
 
   );
