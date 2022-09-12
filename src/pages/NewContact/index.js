61c8d100d3435ba34f3c2aa5f79f90761c7b90fd
Jonyelson Lopes <jonyelson20@gmail.com>
2022-09-12 16:07:09 -0300

    Componentes Input e Select

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,9 +1,22 @@
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 export default function NewContact() {
   return (
-    <PageHeader
-      title="Novo Contato"
-    />
+    <>
+      <PageHeader
+        title="Novo Contato"
+      />
+      <Input placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Facebook</option>
+        <option value="123">Instagram</option>
+      </Select>
+    </>
+
   );
 }
